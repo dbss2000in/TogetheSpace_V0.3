e1636190ed04656d70e38f41899cd73d838abd90
dbss2000in <dbss2000in@gmail.com>
2026-09-01 21:26:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,8 @@ SCOPES = [
 @st.cache_resource
 def get_gspread_client():
   creds_dict = dict(st.secrets["gcp_service_account"])
+  if "private_key" in creds_dict:
+    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
   creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
   return gspread.authorize(creds)
 
